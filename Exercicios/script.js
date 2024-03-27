5231c8820028d07ce2874e4a702b1a34272e6ec0
Bárbara Pereira Silva <126683616+BarbaraPSilva@users.noreply.github.com>
2024-03-27 11:09:13 -0300

    Add files via upload

---
+++ Exercicios/script.js
@@ -0,0 +1,48 @@
+//Crie duas variáveis e atribua valores numéricos a elas.//
+//Use uma terceira variável para armazenar a soma dos dois números.//
+
+//variaveis
+
+function soma(){
+var soma= x+y;
+
+console.log(soma);
+
+}
+//
+
+//Crie duas variáveis e atribua valores numéricos a elas.//
+//Use uma terceira variável para armazenar a diferença entre os dois números.//
+
+//variaveis
+
+
+
+//
+
+//Crie duas variáveis e atribua valores numéricos a elas.//
+//Use uma terceira variável para armazenar o produto dos dois números//
+
+//variaveis
+
+
+
+//
+
+//Crie duas variáveis e atribua valores numéricos a elas.//
+//Use uma terceira variável para armazenar o resultado da divisão do primeiro pelo segundo número.//
+
+//variaveis
+
+
+
+//
+
+//Crie uma variável e atribua a ela um número inteiro.//
+//Escreva uma função para determinar se o número é par ou ímpar.//
+
+//variaveis
+
+
+
+//
