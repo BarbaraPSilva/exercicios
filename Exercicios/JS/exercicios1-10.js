d109f922bfed03903ef8ac1cc375397048b0488b
Bárbara Pereira Silva <126683616+BarbaraPSilva@users.noreply.github.com>
2024-04-03 11:36:29 -0300

    Add files via upload

---
+++ Exercicios/JS/exercicios1-10.js
@@ -0,0 +1,65 @@
+let x =16;
+let y = 4;
+let soma;
+
+ soma = x+y;
+
+ console.log(soma);
+
+let sub;
+
+ sub = x-y;
+
+ console.log(sub);
+
+ let produto;
+
+ produto = x*y;
+
+ console.log(produto);
+
+ let divi;
+
+ divi = x/y;
+
+ console.log(divi);
+
+ let quadrado;
+
+ quadrado = x*x;
+
+ console.log(quadrado);
+
+
+let raiz;
+
+raiz =Math.sqrt(x);
+
+console.log(raiz);
+
+let modulo;
+
+modulo= Math.abs(x);
+
+ console.log(modulo);
+
+
+if (x % 2 === 0){
+    console.log('par');
+} else {
+    console.log('impar');
+}
+
+
+let PosNeg;
+
+ PosNeg =Math.sign(x);
+
+ console.log(PosNeg);
+
+
+if (x>=y){
+    console.log('X é maior');
+} else {
+    console.log('Y é maior');
+}
