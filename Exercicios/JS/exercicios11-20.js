12855986527d66460bfe1667f4a6def358d03797
Bárbara Pereira Silva <126683616+BarbaraPSilva@users.noreply.github.com>
2024-04-04 12:28:21 -0300

    Add files via upload

---
+++ Exercicios/JS/exercicios11-20.js
@@ -0,0 +1,26 @@
+let x =23;
+let y =16;
+
+if (x<=y){
+    console.log('X é menor');
+} else {
+    console.log('Y é menor');
+}
+
+let a= 5;
+let b= 24;
+let c= 19;
+let d= a+b+c;
+
+let media;
+
+media = d/3 ;
+
+console.log(media);
+
+let f =5;
+let fatorial;
+
+fatorial= 120;
+
+console.log(fatorial);
