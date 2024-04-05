a6ab3779f95634a6db592c6a8a3cfdc9841e722c
Bárbara Pereira Silva <126683616+BarbaraPSilva@users.noreply.github.com>
2024-04-05 12:34:04 -0300

    Update exercicios11-20.js

--- Exercicios/JS/exercicios11-20.js
+++ Exercicios/JS/exercicios11-20.js
@@ -18,9 +18,43 @@ media = d/3 ;
 
 console.log(media);
 
-let f =5;
-let fatorial;
+var fatorial = 5;
+var resultado = fatorial;
+for (var i =1; i<fatorial;i++){
+    resultado*= i;
+}
+ console.log (resultado);
 
-fatorial= 120;
+ //12
 
-console.log(fatorial);
+ let numeroDigitado =14;
+ let divisor = 2;
+ while(numeroDigitado%divisor != 0)
+     divisor++;
+ 
+ if(numeroDigitado == divisor)
+     console.log("Número Primo!");
+ else
+     console.log("Número NÃO primo!");
+     
+
+     let numero = 4
+
+for (let i = 1; i <= 10; i++) {
+  console.log(numero * i)
+}
+
+//14
+//
+
+if (x % 3 === 0){
+    console.log('É multiplo de 3');
+} else {
+    console.log('Não é multiplo de 3');
+}
+
+if (x % 5 === 0){
+    console.log('É divisivel por 5');
+} else {
+    console.log('Não é divisivel por 5');
+}
